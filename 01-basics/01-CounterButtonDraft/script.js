09d1e786b06976287f20a7c3daaad338e71c00c9
x9satt <x9satt@gmail.com>
2021-05-19 00:41:09 +0300

    #01.01 CounterButtonDraft

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,21 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // Требуется создать Vue приложение
+
+const app = new Vue({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++
+    }
+  },
+  template: `
+        <button  @click="incrementCount">{{ this.count }}</button>
+    `
+});
+
+app.$mount('#app');
