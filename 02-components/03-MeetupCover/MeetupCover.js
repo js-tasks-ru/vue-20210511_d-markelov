d9b851de8fdabc0be11e0255b421a95c86b66d08
dmark <d.markelov@glovisrus.com>
2021-05-21 15:45:18 +0300

    solution for 02.03 MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,9 +1,24 @@
 const MeetupCover = {
   name: 'MeetupCover',
 
+  props:{
+    title:{
+      type: String,
+    },
+    link:{
+      type: String,
+    },
+  },
+
+  computed:{
+    bgLink(){
+      return this.link ? `--bg-url: url('${this.link}')` : '';
+    }
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="bgLink">
+        <h1 class="meetup-cover__title">{{title}}</h1>
     </div>`,
 };
 
