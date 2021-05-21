ead97c72e279955bba8ecc380fcbea3265968d18
dmark <d.markelov@glovisrus.com>
2021-05-21 15:38:21 +0300

    solution for 02.05 MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,14 +1,22 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 const MeetupAgenda = {
   name: 'MeetupAgenda',
 
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
+
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
-      <div class="meetup-agenda__item"></div>
-      <!-- ... -->
-      <div class="meetup-agenda__item"></div>
+      <meetup-agenda-item v-for="agenda_item in agenda" :agenda-item="agenda_item"></meetup-agenda-item>
     </div>`,
 };
 
--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,40 @@
-// import { agendaItemIcons, agendaItemDefaultTitles } from './data.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './data.js';
 
 const MeetupAgendaItem = {
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    refinedAgendaItem() {
+      return {
+        ...this.agendaItem,
+        title: this.agendaItem.title ? this.agendaItem.title : agendaItemDefaultTitles[this.agendaItem.type],
+        icon: `/assets/icons/icon-${agendaItemIcons[this.agendaItem.type]}.svg`,
+      };
+    },
+  },
+
   template: `
     <div class="meetup-agenda__item">
-      <div class="meetup-agenda__item-col">
-        <img class="icon" alt="icon" src="/assets/icons/icon-cal-sm.svg" />
-      </div>
-      <div class="meetup-agenda__item-col">00:00 - 00:00</div>
-      <div class="meetup-agenda__item-col">
-        <h5 class="meetup-agenda__title">Заголовок</h5>
-        <p>
-          <span>Докладчик</span>
-          <span class="meetup-agenda__dot"></span>
-          <span class="meetup-agenda__lang">ru</span>
-        </p>
-        <p>Описание</p>
-      </div>
+        <div class="meetup-agenda__item-col">
+          <img class="icon" alt="icon" :src="refinedAgendaItem.icon" />
+        </div>
+        <div class="meetup-agenda__item-col">{{refinedAgendaItem.startsAt}} - {{refinedAgendaItem.endsAt}}</div>
+        <div class="meetup-agenda__item-col">
+          <h5 class="meetup-agenda__title">{{refinedAgendaItem.title}}</h5>
+          <p v-if="refinedAgendaItem.type === 'talk'">
+            <span>{{refinedAgendaItem.speaker}}</span>
+            <span class="meetup-agenda__dot"></span>
+            <span class="meetup-agenda__lang">{{refinedAgendaItem.language}}</span>
+          </p>
+          <p v-if="refinedAgendaItem.description">{{refinedAgendaItem.description}}</p>
+        </div>
     </div>`,
 };
 
