5c579300bb248788b43c2f1e7098bac957b8c632
mad <t@t>
2021-05-25 08:25:53 +0300

    solution for 03.01 CounterButton sfc

--- 03-sfc/01-CounterButton-vue/components/CounterButton.vue
+++ 03-sfc/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,27 @@
-<template><div></div></template>
+<template>
+  <div>
+    <button :value="count" type="button" @click="$emit('increment', count + 1)">
+      {{ count }}
+    </button>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: 'CounterButton',
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
 </script>
 
 <style></style>
