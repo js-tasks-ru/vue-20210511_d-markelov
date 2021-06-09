3a06a591e75edc2d0922ebb2e97a3907dde34db3
dmark <d.markelov@glovisrus.com>
2021-06-09 10:35:01 +0300

    solution for 06.01 buttons

--- 06-wrappers/01-Buttons/components/DangerButton.vue
+++ 06-wrappers/01-Buttons/components/DangerButton.vue
@@ -1,10 +1,26 @@
 <template>
-  <button></button>
+  <base-button class="button_danger" :tag="tag" :block="block" v-bind="$attrs" v-on="$listeners"><slot /></base-button>
 </template>
 
 <script>
+import BaseButton from './BaseButton.vue';
+
 export default {
   name: 'DangerButton',
+
+  components: {
+    BaseButton,
+  },
+
+  inheritAttrs: false,
+
+  props: {
+    block: Boolean,
+    tag: {
+      type: String,
+      default: 'button',
+    },
+  },
 };
 </script>
 
--- 06-wrappers/01-Buttons/components/SecondaryButton.vue
+++ 06-wrappers/01-Buttons/components/SecondaryButton.vue
@@ -1,10 +1,25 @@
 <template>
-  <button></button>
+  <base-button class="button_secondary" :tag="tag" :block="block" v-bind="$attrs" v-on="$listeners"><slot /></base-button>
 </template>
 
 <script>
+import BaseButton from './BaseButton.vue';
 export default {
   name: 'SecondaryButton',
+
+  components: {
+    BaseButton,
+  },
+
+  inheritAttrs: false,
+
+  props: {
+    block: Boolean,
+    tag: {
+      type: String,
+      default: 'button',
+    },
+  },
 };
 </script>
 
