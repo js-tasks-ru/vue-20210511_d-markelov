2dbbbca51ccad52be055e81cedad70bdc226bc2e
x9satt <x9satt@gmail.com>
2021-06-21 00:38:52 +0300

    solution for 08.02 NodesPagination

--- 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
+++ 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
@@ -1,5 +1,16 @@
 <script>
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: Number,
+    perPage: Number,
+  },
+
+  render: function (h) {
+    return this.$slots.default
+      ? h('div', this.$slots.default.slice((this.page - 1) * this.perPage, this.page * this.perPage))
+      : null;
+  },
 };
 </script>
